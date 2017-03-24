5dedf28116bdab51595414172d568bf16bb50c43
Vinod Kumar <mac.vinod.kumar@gmail.com>
2017-03-24 09:59:07 +0530

    Initial Commit

---
+++ vCardType/vCardType.h
@@ -0,0 +1,19 @@
+//
+//  vCardType.h
+//  vCardType
+//
+//  Created by MAC on 24/03/17.
+//  Copyright © 2017 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for vCardType.
+FOUNDATION_EXPORT double vCardTypeVersionNumber;
+
+//! Project version string for vCardType.
+FOUNDATION_EXPORT const unsigned char vCardTypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vCardType/PublicHeader.h>
+
+
